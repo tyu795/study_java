777a47e308ccf48b10543db0aedda13015cfe85a
tyu795 <tyu795@naver.com>
2023-05-24 17:18:54 +0900

    2023.05.24

---
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("하이");
+		
+		boolean a = true;
+		boolean b = false;
+		
+		int c = 1;
+		
+	
+	}
+
+}
