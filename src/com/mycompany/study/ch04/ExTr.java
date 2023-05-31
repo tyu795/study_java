fa6e2d945048084daeba8fdb91d66835e18fef5d
tyu795 <tyu795@naver.com>
2023-05-31 17:21:41 +0900

    2023.05.31

---
+++ src/com/mycompany/study/ch04/ExTr.java
@@ -0,0 +1,20 @@
+package com.mycompany.study.ch04;
+
+public class ExTr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		
+		if (a != 0) {
+			if (a%2 == 0) {
+				System.out.println("짝수");
+			} else {
+				System.out.println("홀수");
+			}
+			} else {
+				System.out.println("0");
+			}
+		}
+
+	}
