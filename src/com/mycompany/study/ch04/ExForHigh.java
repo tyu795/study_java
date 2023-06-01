70481db9acd0522b1065a5c2e8bca5510816ec78
tyu795 <tyu795@naver.com>
2023-06-01 18:14:00 +0900

    2023.06.01

--- src/com/mycompany/study/ch04/ExForHigh.java
+++ src/com/mycompany/study/ch04/ExForHigh.java
@@ -13,11 +13,7 @@ public class ExForHigh {
 	       
 		 	}
 	
-			/*
-			 * for (int i = 1; i <= 1; i++) { for(int j = 1; j <= 10; j++) {
-			 * System.out.print("*"); } System.out.println(); } for(int i=1; i<=5;i++) {
-			 * for(int j=1; j<=10;j++) { System.out.print("*"); } System.out.println(); }
-			 */
+			
 		 	// 박스 만들기
 			System.out.println("-------------------");
 			
